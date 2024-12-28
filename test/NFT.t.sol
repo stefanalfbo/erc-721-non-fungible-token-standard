bb4be8295a456eec96e1b5ed2d8d22356cab9951
Stefan Alfbo <stefan@alfbo.se>
2024-12-28 10:12:56 +0100

    Create a skeleton test contract

---
+++ test/NFT.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {NFT} from "../src/NFT.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract NFTTest is Test {
+    function testTemplate() public {}
+}
